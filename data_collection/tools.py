120a59fd081ae27161fcdccab6a4901b87cce4c0
Dainius <kirsnauskas1@gmail.com>
2024-02-27 11:18:15 +0200

    fix

--- data_collection/tools.py
+++ data_collection/tools.py
@@ -1,5 +1,6 @@
-import requests
 import os
+import requests
+from requests.exceptions import RequestException
 import pandas as pd
 from pandas import DataFrame
 
@@ -116,7 +117,6 @@ LOVD_VARIABLES_DATA_TYPES = {
     'Individual/Origin/Population': 'String',
     'Individual/Individual_ID': 'String',
     'allele': 'Integer',
-    'chromosome': 'Integer',
     'position_g_start': 'Integer',
     'position_g_end': 'Integer',
     'type': 'String',
@@ -154,37 +154,27 @@ def get_file_from_url(url, save_to, override=False):
     :param bool override: needs override
     """
 
+    # check if directory exists, if not - create
+    save_to_dir = os.path.dirname(save_to)
+    if not os.path.exists(save_to_dir):
+        os.makedirs(save_to_dir)
+
+    # check if file exist and needs to override
+    if os.path.exists(save_to) and not override:
+        print(f"The file at {save_to} already exists.")
+        return
+
     try:
-        # check if directory exists, if not - create
-        save_to_dir = os.path.dirname(save_to)
-        if not os.path.exists(save_to_dir):
-            os.makedirs(save_to_dir)
+        response = requests.get(url, timeout=10)
+    except RequestException as e:
+        raise DownloadError(f"Error while downloading file from {url}") from e
 
-        # check if file exist and needs to override
-        if os.path.exists(save_to) and not override:
-            print(f"The file at {save_to} already exists.")
-            return
+    if response.status_code != 200:
+        raise BadResponseException(f"Bad response from {url}."
+                                   f" Status code: {response.status_code}")
 
-        try:
-            response = requests.get(url)
-        except requests.exceptions.RequestException as e:
-            raise DownloadError(f"Error downloading file from {url}: {e}")
-
-        if response.status_code != 200:
-            raise BadResponseException(f"Bad response from {url}. Status code: {response.status_code}")
-
-        with open(save_to, "wb") as f:
-            f.write(response.content)
-
-    # check request exceptions
-    except BadResponseException as e:
-        print(f"Error: {e}")
-
-    except DownloadError as e:
-        print(f"Error: {e}")
-
-    except Exception as e:
-        print(f"Error: {e}")
+    with open(save_to, "wb") as f:
+        f.write(response.content)
 
 
 def convert_lovd_data_types(frame, table_name):
@@ -232,7 +222,7 @@ def from_lovd_to_pandas(path):
         if not os.path.exists(path):
             raise FileNotFoundError(f"The file at {path} does not exist.")
 
-        d = dict()
+        d = {}
 
         with open(path) as f:
             # skip header
