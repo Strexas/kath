ac62432a2d676030d4342c9a93be292e9be1889c
Gerdagithub <gerdaliukas98@gmail.com>
2024-04-15 21:22:33 +0300

    logging a message when folder is created

--- data_collection/collection.py
+++ data_collection/collection.py
@@ -45,6 +45,7 @@ def get_file_from_url(url, save_to, override=False):
     directory = os.path.dirname(save_to)
     if not os.path.exists(directory):
         os.makedirs(directory)
+        logging.info(f"Created directory: {directory}")
 
     # check if file exist and needs to override
     if os.path.exists(save_to) and not override:
