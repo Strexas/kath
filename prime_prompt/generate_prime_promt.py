8dd1f3303caed1a0d4dfecad57605166f0cd9b32
Dainius Kirsnauskas <kirsnauskas@gmail.com>
2024-05-01 17:19:30 +0300

    Prime prompt generator

--- prime_prompt/generate_prime_promt.py
+++ prime_prompt/generate_prime_promt.py
@@ -1,17 +1,18 @@
+""" Script for generating prime prompt """
+
 import os
 from os.path import normpath, join
 
-
 project_folder = normpath(join(__file__, '..', '..'))
+
 
 def scan_docs(path: str):
     """
     Scans Python file and yields function and its documentation
     :param path: path of file
     """
-    with open(path, "r") as file:
-
-        lines = file.readlines()
+    with open(path, "r", encoding="utf=8") as python_file:
+        lines = python_file.readlines()
         i = 0
         while i < len(lines):
             line = lines[i]
@@ -19,21 +20,20 @@ def scan_docs(path: str):
             if not line.startswith("def ") or line[4] == "_":
                 continue
 
-
-            function = line[4:line.index("(")]
+            function_name = line[4:line.index("(")]
             i += 1  # skip first """
 
-            documentation = ""
+            function_documentation = ""
             while True:
                 if lines[i].strip() == "\"\"\"":
                     break
-                documentation += lines[i]
+                function_documentation += lines[i]
                 i += 1
-            yield function, documentation
+            yield function_name, documentation
 
 
 llm_related_modules = ["data_collection"]
-with open("prime_prompt.txt", "w") as f:
+with open("prime_prompt.txt", "w", encoding="utf=8") as f:
     for module in llm_related_modules:
         for directory, folder, files in os.walk(join(project_folder, module)):
             for file in files:
