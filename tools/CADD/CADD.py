a202d461dd4431da0593ca59cfb062f5eb9aa2b4
Nojux-oficial <nojus.sajauskas@gmail.com>
2024-05-10 10:02:53 +0300

    documentation of fetch_cadd_score function

--- tools/CADD/CADD.py
+++ tools/CADD/CADD.py
@@ -1,6 +1,14 @@
 import requests
 
 def fetch_cadd_score(cadd_version, chromosome, position):
+    """
+    Fetches a single SNV (Single Nucleotide Variant) score from the Combined Annotation Dependent Depletion (CADD) tool.
+    
+    :param str cadd_version: Version of the CADD model used, e.g., "v1.3" or "GRCh38-v17.7".
+    :param int chromosome: Chromosome number where the SNV is located.
+    :param int position: Genomic position of the SNV on the chromosome.
+    :return: A dictionary containing CADD scores and annotations for the specified SNV, or None if an error occurs.
+    """
     url = f"https://cadd.gs.washington.edu/api/v1.0/{cadd_version}/{chromosome}:{position}"
     try:
         response = requests.get(url)
