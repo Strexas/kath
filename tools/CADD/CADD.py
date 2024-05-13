b49f1e73787ff6261730fc169c7ad01235ee0c0c
Nojux-oficial <nojus.sajauskas@gmail.com>
2024-05-13 22:28:34 +0300

    Fixing long lines

--- tools/CADD/CADD.py
+++ tools/CADD/CADD.py
@@ -3,12 +3,14 @@ import argparse
 
 def fetch_cadd_score(cadd_version, chromosome, position):
     """
-    Fetches a single SNV (Single Nucleotide Variant) score from the Combined Annotation Dependent Depletion (CADD) tool.
+    Fetches a single SNV (Single Nucleotide Variant) score
+    from the Combined Annotation Dependent Depletion (CADD) tool.
     
     :param str cadd_version: Version of the CADD model used, e.g., "v1.3" or "GRCh38-v1.7".
     :param int chromosome: Chromosome number where the SNV is located.
     :param int position: Genomic position of the SNV on the chromosome.
-    :return: A dictionary containing CADD scores and annotations for the specified SNV, or None if an error occurs.
+    :return: A dictionary containing CADD scores and annotations
+    for the specified SNV, or None if an error occurs.
     """
     url = f"https://cadd.gs.washington.edu/api/v1.0/{cadd_version}/{chromosome}:{position}"
     try:
@@ -31,7 +33,8 @@ def fetch_cadd_scores(cadd_version, chromosome, start, end):
     :param int chromosome: Chromosome number for the genomic region.
     :param int start: Genomic start position of the region.
     :param int end: Genomic end position of the region.
-    :return: A dictionary containing CADD scores and annotations for the specified genomic region, or None if an error occurs.
+    :return: A dictionary containing CADD scores and annotations
+    for the specified genomic region, or None if an error occurs.
     """
     url = f"https://cadd.gs.washington.edu/api/v1.0/{cadd_version}/{chromosome}:{start}-{end}"
     try:
@@ -51,7 +54,8 @@ if __name__ == "__main__":
     parser.add_argument("version", help="CADD version, e.g., 'v1.3' or 'GRCh38-v1.7'")
     parser.add_argument("chromosome", type=int, help="Chromosome number")
     parser.add_argument("--position", type=int, help="Genomic position (for single SNV)")
-    parser.add_argument("--start", type=int, help="Genomic start position (for a range of positions)")
+    parser.add_argument("--start", type=int,
+                        help="Genomic start position (for a range of positions)")
     parser.add_argument("--end", type=int, help="Genomic end position (for a range of positions)")
 
     args = parser.parse_args()
@@ -63,4 +67,5 @@ if __name__ == "__main__":
         result = fetch_cadd_scores(args.version, args.chromosome, args.start, args.end)
         print(result)
     else:
-        print("Please provide either '--position' for single SNV or '--start' and '--end' for a range of positions.")
+        print("Please provide either '--position' for single SNV \
+              or '--start' and '--end' for a range of positions.")
