2e81ca4f75289ee51dd8af07f2215ac6be0b3ac0
Nojux-oficial <nojus.sajauskas@gmail.com>
2024-05-13 22:35:43 +0300

    Removing unnecessary else statements

--- tools/CADD/CADD.py
+++ tools/CADD/CADD.py
@@ -21,9 +21,8 @@ def fetch_cadd_score(cadd_version, chromosome, position):
         if response.status_code == 200:
             data = response.json()
             return data
-        else:
-            print(f"Error: {response.status_code} - {response.text}")
-            return None
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
     except Exception as e:
         print(f"Error: {e}")
         return None
@@ -39,16 +38,15 @@ def fetch_cadd_scores(cadd_version, chromosome, start, end):
     :return: A dictionary containing CADD scores and annotations
     for the specified genomic region, or None if an error occurs.
     """
-    
+
     url = f"https://cadd.gs.washington.edu/api/v1.0/{cadd_version}/{chromosome}:{start}-{end}"
     try:
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
             return data
-        else:
-            print(f"Error: {response.status_code} - {response.text}")
-            return None
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
     except Exception as e:
         print(f"Error: {e}")
         return None
