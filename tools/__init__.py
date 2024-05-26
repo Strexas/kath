6ee84022ebeb41808827c125b65c577d92053046
Nojux-oficial <nojus.sajauskas@gmail.com>
2024-05-26 22:27:08 +0300

    Docstring for tools package (__init__)

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,6 +1,10 @@
+"""
+This module provides access to the tools for fetching scores for genetic variants.
+"""
+
 # CADD IMPORT
 from .cadd.cadd import (
     # SNV scoring
     fetch_cadd_score,
-    fetch_cadd_scores   
+    fetch_cadd_scores
 )
