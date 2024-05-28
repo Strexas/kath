e8a70b902a698eecd47dbd38813412b08c5cc3e4
Dainius Kirsnauskas <kirsnauskas@gmail.com>
2024-05-28 22:56:43 +0300

    openai_api

--- GUI/back-end/main.py
+++ GUI/back-end/main.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+import openai
+from pygments.lexers import q
 
 from router import router_bp
 
@@ -26,6 +28,22 @@ app.register_blueprint(router_bp('/api/v1'))
 
 # Configurations
 cors = CORS(app, origins=origins)
+
+@app.route('/api/v1/request', methods=['POST'])
+def process():
+  request_data = request.get_json()
+  if not request_data:
+    return ''
+
+  openai.api_key = "sk-proj-FCpUZQtdF7z9gBjMfbgVT3BlbkFJUSgsIUYLbmGrfcZ3SJ4k"
+
+
+  response = openai.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": request_data}],
+  )
+
+  return response.choices[0].message.content
 
 # Run the app
 if __name__ == '__main__':
