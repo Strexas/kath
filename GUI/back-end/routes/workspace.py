b43a004f5933942cdca8a0d185bf5988a28ae028
Paulius Preikša <preiksap@gmail.com>
2024-06-13 18:48:33 +0300

    feat: added workspace delete

--- GUI/back-end/routes/workspace.py
+++ GUI/back-end/routes/workspace.py
@@ -32,3 +32,19 @@ def get_workspace(workspace):
     return make_response(jsonify({'files': all_files}), 200)
   else:
     return make_response(jsonify({'message': 'Workspace not found!'}), 404)
+  
+@route_workspace_bp.route('/workspace/<workspace>', methods=['DELETE'])
+def delete_workspace(workspace):
+  path = f"{PATH_TO_WORKSPACE}/{workspace}"
+  if os.path.exists(path) and os.path.isdir(path):
+    delete_all_files_in_directory(path)
+    return make_response(jsonify({'message': 'Workspace deleted!'}), 200)
+  else:
+    return make_response(jsonify({'message': 'Workspace not found!'}), 404)
+  
+def delete_all_files_in_directory(path):
+  for file_name in os.listdir(path):
+    file_path = os.path.join(path, file_name)
+    if os.path.isfile(file_path):
+      os.remove(file_path)
+  os.rmdir(path)
