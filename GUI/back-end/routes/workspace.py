f4fc666af9b101243976f383f4367cedcba6f93a
Paulius Preikša <preiksap@gmail.com>
2024-06-18 16:45:04 +0300

    feat: added file upload/delete

--- GUI/back-end/routes/workspace.py
+++ GUI/back-end/routes/workspace.py
@@ -41,7 +41,38 @@ def delete_workspace(workspace):
     return make_response(jsonify({'message': 'Workspace deleted!'}), 200)
   else:
     return make_response(jsonify({'message': 'Workspace not found!'}), 404)
+
+@route_workspace_bp.route('/workspace/<workspace>/file', methods=['POST'])
+def upload_file(workspace):
+
+  if 'file' not in request.files:
+    return make_response(jsonify({'message': 'No file part in the request'}), 400)
+
+  file = request.files['file']
+
+  if file.filename == '':
+    return make_response(jsonify({'message': 'No file selected for uploading'}), 400)
   
+  if not os.path.exists(f"{PATH_TO_WORKSPACE}/{workspace}"):
+    return make_response(jsonify({'message': 'Workspace not found!'}), 404)
+  
+  if file and file.filename.endswith('.txt'):
+    path = f"{PATH_TO_WORKSPACE}/{workspace}/{file.filename}"
+    file.save(path)
+    return make_response(jsonify({'message': 'File successfully uploaded'}), 200)
+
+  return make_response(jsonify({'message': 'File must be a .txt file'}), 400)
+  
+@route_workspace_bp.route('/workspace/<workspace>/file', methods=['DELETE'])
+def delete_file(workspace):
+  file_name = request.json.get('file_name')
+  path = f"{PATH_TO_WORKSPACE}/{workspace}/{file_name}"
+  if os.path.exists(path) and os.path.isfile(path):
+    os.remove(path)
+    return make_response(jsonify({'message': 'File deleted!'}), 200)
+  else:
+    return make_response(jsonify({'message': 'File not found!'}), 404)
+
 def delete_all_files_in_directory(path):
   for file_name in os.listdir(path):
     file_path = os.path.join(path, file_name)
