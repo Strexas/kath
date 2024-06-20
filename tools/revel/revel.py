b33fca96ebdb542957c0f42b92697dbe04c3f15c
nojux-official <nojus.sajauskas@gmail.com>
2024-06-20 15:15:11 +0000

    Function's description

--- tools/revel/revel.py
+++ tools/revel/revel.py
@@ -1,3 +1,5 @@
+""" Retrieves REVEL scores for specific variants based on chromosome and position from a CSV file. """
+
 import pandas as pd
 import os
 
@@ -5,6 +7,13 @@ current_script_dir = os.path.dirname(os.path.abspath(__file__))
 revel_file = os.path.join(current_script_dir, 'revel_with_transcript_ids')
 
 def get_revel_scores(chromosome, position):
+    """
+    Retrieve REVEL scores for variants at a specific chromosome and position.
+    
+    :param int chromosome: Chromosome number to filter the variants.
+    :param int position: Sequence position (hg19) to filter the variants.
+    :return: A pandas DataFrame containing the filtered variants with REVEL scores.
+    """
     variants = []
     revel_data = pd.read_csv(revel_file)
     
