34bdc4a9925d5a1f0e34a87fa350f2585dab3ce6
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-08-20 23:57:07 +0300

    MDE/PKFE-22 implemented filebar view

---
+++ app/front-end/src/features/editor/components/filebarView/index.ts
@@ -0,0 +1,5 @@
+export { FilebarGroup } from './filebarGroup';
+export type { FilebarGroupProps } from './filebarGroup';
+export { FilebarGroupItem } from './filebarGroupItem';
+export type { FilebarGroupItemProps } from './filebarGroupItem';
+export { FilebarView } from './filebarView';
