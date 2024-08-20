ebc56c7af66fe19f048a3bb58d4943578ecf578f
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-08-20 17:31:19 +0300

    MDE/PKFE-7 exporting components

---
+++ app/front-end/src/features/editor/components/toolbarView/index.ts
@@ -0,0 +1,5 @@
+export { ToolbarGroup, ToolbarGroupItem } from './toolbarGroup';
+export type { ToolbarGroupItemProps, ToolbarGroupProps } from './toolbarGroup';
+export { ToolbarGroupsSelector, ToolbarGroupsSelectorItem } from './toolbarGroupSelector';
+export type { ToolbarGroupsSelectorItemProps, ToolbarGroupsSelectorProps } from './toolbarGroupSelector';
+export { ToolbarView } from './toolbarView';
