c27b5523f47c1e918b95fd3b23251ebf4b5cb936
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-09-08 00:41:14 +0300

    MDE/PKFE-31 implemented toolbar context provider

--- app/front-end/src/features/editor/stores/index.ts
+++ app/front-end/src/features/editor/stores/index.ts
@@ -1,2 +1,4 @@
+export { ToolbarContext, ToolbarContextProvider } from './toolbarContextProvider';
+export type { ToolbarContextProps } from './toolbarContextProvider';
 export { WorkspaceContext, WorkspaceContextProvider } from './workspaceContextProvider';
 export type { WorkspaceContextProps } from './workspaceContextProvider';
