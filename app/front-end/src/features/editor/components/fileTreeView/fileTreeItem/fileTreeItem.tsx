4f9d1ecdcf909702f9de1f141c24db9486983ab3
Justinas <156369263+justinnas@users.noreply.github.com>
2024-09-15 21:11:02 +0300

    Fixed a bug where a popup would appear when clicked on a folder

--- app/front-end/src/features/editor/components/fileTreeView/fileTreeItem/fileTreeItem.tsx
+++ app/front-end/src/features/editor/components/fileTreeView/fileTreeItem/fileTreeItem.tsx
@@ -183,7 +183,7 @@ export const FileTreeItem = React.forwardRef(function CustomTreeItem(
               if (!blocked) {
                 if (getContentProps().onClick) getContentProps().onClick(event);
                 if (unsaved) {
-                  setIsConfirmDialogOpen(true);
+                  if (item.fileType !== FileTypes.FOLDER) setIsConfirmDialogOpen(true);
                 } else {
                   handleClick(item.id, item.label, item.fileType);
                 }
