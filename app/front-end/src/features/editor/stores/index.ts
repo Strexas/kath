88a5785b20af3b4b137a4c1fe1b995ae727c5cf3
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-10-10 01:42:53 +0300

    MDE/PKFE-50 refactored toolbar context

--- app/front-end/src/features/editor/stores/index.ts
+++ app/front-end/src/features/editor/stores/index.ts
@@ -1,4 +1,4 @@
-export { ToolbarContext, ToolbarContextProvider } from './toolbarContextProvider';
+export { ToolbarContext, ToolbarContextProvider, defaultSaveTo } from './toolbarContextProvider';
 export type { ToolbarContextProps } from './toolbarContextProvider';
 export { WorkspaceContext, WorkspaceContextProvider } from './workspaceContextProvider';
 export type { WorkspaceContextProps } from './workspaceContextProvider';
