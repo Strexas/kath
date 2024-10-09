f1f0e1b9e1d0cadbec70d6cd9ada9ebbc0a22c35
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-10-10 01:45:48 +0300

    MDE/PKFE-50 implemented autocomplete into merge params field

--- app/front-end/src/features/editor/components/toolbarView/toolbarGroupParams/mergeGroupParams.tsx
+++ app/front-end/src/features/editor/components/toolbarView/toolbarGroupParams/mergeGroupParams.tsx
@@ -1,24 +1,21 @@
+import { Autocomplete, TextField } from "@mui/material";
 import {
-  GroupParamsInputLabel,
   GroupParamsTypography,
-  StyledGroupParamsListSubheader,
-  StyledGroupParamsMenuItem,
   StyledGroupParamsMenuItemTypography,
   StyledGroupParamsMenuItemTypographyBold,
-  StyledGroupParamsSelect,
 } from '@/features/editor/components/toolbarView/toolbarGroupParams';
 import { useToolbarContext, useWorkspaceContext } from '@/features/editor/hooks';
 import { FileModel, FileTypes } from '@/features/editor/types';
 import { getWorkspaceArray } from '@/features/editor/utils';
 import { useStatusContext } from '@/hooks';
-import { Box, Checkbox, FormControl, FormControlLabel, SelectChangeEvent, useTheme } from '@mui/material';
+import { Box, Checkbox, FormControlLabel } from '@mui/material';
 import { useEffect, useState } from 'react';
+import { defaultSaveTo } from "@/features/editor/stores";
 
 export interface MergeGroupParamsProps {}
 
 export const MergeGroupParams: React.FC<MergeGroupParamsProps> = () => {
   const { blocked } = useStatusContext();
-  const Theme = useTheme();
   const { fileTree, fileTreeArray } = useWorkspaceContext();
   const {
     saveTo,
@@ -38,25 +35,25 @@ export const MergeGroupParams: React.FC<MergeGroupParamsProps> = () => {
   //
   // Merge state
   //
-  const [lovdFileValue, setLovdFileValue] = useState<string>(lovdFile);
-  const [clinvarFileValue, setClinvarFileValue] = useState<string>(clinvarFile);
-  const [gnomadFileValue, setGnomadFileValue] = useState<string>(gnomadFile);
+  const [lovdFileState, setLovdFileState] = useState<FileModel | null>(lovdFile);
+  const [clinvarFileState, setClinvarFileState] = useState<FileModel | null>(clinvarFile);
+  const [gnomadFileState, setGnomadFileState] = useState<FileModel | null>(gnomadFile);
 
-  const handleLovdFileChange = (event: SelectChangeEvent<any>) => {
-    setLovdFileValue(event.target.value);
-    mergeStateUpdate(event.target.value, clinvarFileValue, gnomadFileValue);
+  const handleLovdFileChange = (value: FileModel) => {
+    setLovdFileState(value);
+    mergeStateUpdate(value, undefined, undefined);
     lovdErrorStateUpdate('');
   };
 
-  const handleClinvarFileChange = (event: SelectChangeEvent<any>) => {
-    setClinvarFileValue(event.target.value);
-    mergeStateUpdate(lovdFileValue, event.target.value, gnomadFileValue);
+  const handleClinvarFileChange = (value: FileModel) => {
+    setClinvarFileState(value);
+    mergeStateUpdate(undefined, value, undefined);
     clinvarErrorStateUpdate('');
   };
 
-  const handleGnomadFileChange = (event: SelectChangeEvent<any>) => {
-    setGnomadFileValue(event.target.value);
-    mergeStateUpdate(lovdFileValue, clinvarFileValue, event.target.value);
+  const handleGnomadFileChange = (value: FileModel) => {
+    setGnomadFileState(value);
+    mergeStateUpdate(undefined, undefined, value);
     gnomadErrorStateUpdate('');
   };
 
@@ -64,18 +61,18 @@ export const MergeGroupParams: React.FC<MergeGroupParamsProps> = () => {
   // Save To state
   //
   const [fileArray, setFileArray] = useState<FileModel[]>(fileTreeArray);
-  const [saveToValue, setSaveToValue] = useState<string>(saveTo);
-  const [overrideValue, setOverrideValue] = useState<boolean>(false);
+  const [saveToState, setSaveToState] = useState<FileModel>(saveTo);
+  const [overrideState, setOverrideState] = useState<boolean>(false);
 
-  const handleSaveToChange = (event: SelectChangeEvent<any>) => {
-    setSaveToValue(event.target.value);
-    saveToStateUpdate(event.target.value, false);
-    setOverrideValue(false);
+  const handleSaveToChange = (value: FileModel) => {
+    setSaveToState(value);
+    saveToStateUpdate(value, false);
+    setOverrideState(false);
   };
 
   const handleOverrideChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setOverrideValue(event.target.checked);
-    saveToStateUpdate(saveToValue, event.target.checked);
+    setOverrideState(event.target.checked);
+    saveToStateUpdate(saveToState, event.target.checked);
   };
 
   //
@@ -86,194 +83,161 @@ export const MergeGroupParams: React.FC<MergeGroupParamsProps> = () => {
   }, [fileTree]);
 
   return (
-    <Box sx={{ display: 'grid', gridTemplateColumns: '50% 50%', p: '1rem' }}>
-      <Box
-        sx={{
-          height: '100%',
-          flexDirection: 'column',
-          alignItems: 'start',
-          display: 'grid',
-          gap: '1rem',
-        }}
-      >
-        <FormControl sx={{ width: '90%' }} size='small'>
-          <GroupParamsInputLabel label={'Lovd File'} error={lovdError} />
-          <StyledGroupParamsSelect
-            id={'lovd-file-select'}
-            name={'lovd-file-select'}
-            label={'Lovd File'}
-            value={lovdFileValue}
-            onChange={handleLovdFileChange}
-            error={Boolean(lovdError)}
-            disabled={blocked}
-            MenuProps={{
-              PaperProps: {
-                sx: {
-                  maxWidth: '15.7rem',
-                  maxHeight: '20rem',
-                  bgcolor: Theme.palette.background.default,
-                },
-              },
-            }}
-          >
-            <StyledGroupParamsListSubheader key={'root'}>
-              <StyledGroupParamsMenuItemTypographyBold>root:</StyledGroupParamsMenuItemTypographyBold>
-            </StyledGroupParamsListSubheader>
-            {fileArray.map((file) => {
-              if (file.type === FileTypes.FOLDER) {
-                return (
-                  <StyledGroupParamsListSubheader key={file.id}>
-                    <StyledGroupParamsMenuItemTypographyBold>{file.id}:</StyledGroupParamsMenuItemTypographyBold>
-                  </StyledGroupParamsListSubheader>
-                );
-              }
-
-              return (
-                <StyledGroupParamsMenuItem key={file.id} value={file.id}>
-                  <StyledGroupParamsMenuItemTypography>{file.label}</StyledGroupParamsMenuItemTypography>
-                </StyledGroupParamsMenuItem>
-              );
-            })}
-          </StyledGroupParamsSelect>
-        </FormControl>
-        <FormControl sx={{ width: '90%' }} size='small'>
-          <GroupParamsInputLabel label={'Clinvar File'} error={clinvarError} />
-          <StyledGroupParamsSelect
-            id={'clinvar-file-select'}
-            name={'clinvar-file-select'}
-            label={'Clinvar File'}
-            value={clinvarFileValue}
-            onChange={handleClinvarFileChange}
-            error={Boolean(clinvarError)}
-            disabled={blocked}
-            MenuProps={{
-              PaperProps: {
-                sx: {
-                  maxWidth: '15.7rem',
-                  maxHeight: '20rem',
-                  bgcolor: Theme.palette.background.default,
-                },
-              },
-            }}
-          >
-            <StyledGroupParamsListSubheader key={'root'}>
-              <StyledGroupParamsMenuItemTypographyBold>root:</StyledGroupParamsMenuItemTypographyBold>
-            </StyledGroupParamsListSubheader>
-            {fileArray.map((file) => {
-              if (file.type === FileTypes.FOLDER) {
-                return (
-                  <StyledGroupParamsListSubheader key={file.id}>
-                    <StyledGroupParamsMenuItemTypographyBold>{file.id}:</StyledGroupParamsMenuItemTypographyBold>
-                  </StyledGroupParamsListSubheader>
-                );
-              }
-
-              return (
-                <StyledGroupParamsMenuItem key={file.id} value={file.id}>
-                  <StyledGroupParamsMenuItemTypography>{file.label}</StyledGroupParamsMenuItemTypography>
-                </StyledGroupParamsMenuItem>
-              );
-            })}
-          </StyledGroupParamsSelect>
-        </FormControl>
-        <FormControl sx={{ width: '90%' }} size='small'>
-          <GroupParamsInputLabel label={'Gnomad File'} error={gnomadError} />
-          <StyledGroupParamsSelect
-            id={'gnomad-file-select'}
-            name={'gnomad-file-select'}
-            label={'Gnomad File'}
-            value={gnomadFileValue}
-            onChange={handleGnomadFileChange}
-            error={Boolean(gnomadError)}
-            disabled={blocked}
-            MenuProps={{
-              PaperProps: {
-                sx: {
-                  maxWidth: '15.7rem',
-                  maxHeight: '20rem',
-                  bgcolor: Theme.palette.background.default,
-                },
-              },
-            }}
-          >
-            <StyledGroupParamsListSubheader key={'root'}>
-              <StyledGroupParamsMenuItemTypographyBold>root:</StyledGroupParamsMenuItemTypographyBold>
-            </StyledGroupParamsListSubheader>
-            {fileArray.map((file) => {
-              if (file.type === FileTypes.FOLDER) {
-                return (
-                  <StyledGroupParamsListSubheader key={file.id}>
-                    <StyledGroupParamsMenuItemTypographyBold>{file.id}:</StyledGroupParamsMenuItemTypographyBold>
-                  </StyledGroupParamsListSubheader>
-                );
-              }
-
-              return (
-                <StyledGroupParamsMenuItem key={file.id} value={file.id}>
-                  <StyledGroupParamsMenuItemTypography>{file.label}</StyledGroupParamsMenuItemTypography>
-                </StyledGroupParamsMenuItem>
-              );
-            })}
-          </StyledGroupParamsSelect>
-        </FormControl>
+    <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '1rem', p: '1rem'}}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', rowGap: '1rem', width: '50%'}}>
+        <Autocomplete
+          size="small"
+          sx={(theme) => ({
+            '& fieldset': {
+              borderColor: theme.palette.text.primary,
+              borderRadius: '1rem',
+            },
+          })}
+          value={lovdFileState}
+          onChange={(_event, value) => {
+            if (value)
+              handleLovdFileChange(value)
+          }}
+          disabled={blocked}
+          options={fileArray.filter((file) => file.type !== FileTypes.FOLDER)}
+          groupBy={(option) => option.parent?.id || 'root'}
+          getOptionLabel={(option) => option.label}
+          isOptionEqualToValue={(option, value) => option.id === value.id}
+          renderInput={(params) => 
+            <TextField
+              {...params}
+              label="Lovd File"
+              error={Boolean(lovdError)}
+            />
+          }
+          renderGroup={(params) => (
+            <li key={params.key}>
+              <Box sx={{ px: '0.5rem' }}>
+                <StyledGroupParamsMenuItemTypographyBold>{`${params.group}:`}</StyledGroupParamsMenuItemTypographyBold>
+              </Box>
+              <StyledGroupParamsMenuItemTypography>{params.children}</StyledGroupParamsMenuItemTypography>
+            </li>
+          )}
+        />
+        <Autocomplete
+          size="small"
+          sx={(theme) => ({
+            '& fieldset': {
+              borderColor: theme.palette.text.primary,
+              borderRadius: '1rem',
+            },
+          })}
+          value={clinvarFileState}
+          onChange={(_event, value) => {
+            if (value)
+              handleClinvarFileChange(value)
+          }}
+          disabled={blocked}
+          options={fileArray.filter((file) => file.type !== FileTypes.FOLDER)}
+          groupBy={(option) => option.parent?.id || 'root'}
+          getOptionLabel={(option) => option.label}
+          isOptionEqualToValue={(option, value) => option.id === value.id}
+          renderInput={(params) => 
+            <TextField
+              {...params}
+              label="Clinvar File"
+              error={Boolean(clinvarError)}
+            />
+          }
+          renderGroup={(params) => (
+            <li key={params.key}>
+              <Box sx={{ px: '0.5rem' }}>
+                <StyledGroupParamsMenuItemTypographyBold>{`${params.group}:`}</StyledGroupParamsMenuItemTypographyBold>
+              </Box>
+              <StyledGroupParamsMenuItemTypography>{params.children}</StyledGroupParamsMenuItemTypography>
+            </li>
+          )}
+        />
+        <Autocomplete
+          size="small"
+          sx={(theme) => ({
+            '& fieldset': {
+              borderColor: theme.palette.text.primary,
+              borderRadius: '1rem',
+            },
+          })}
+          value={gnomadFileState}
+          onChange={(_event, value) => {
+            if (value)
+              handleGnomadFileChange(value)
+          }}
+          disabled={blocked}
+          options={fileArray.filter((file) => file.type !== FileTypes.FOLDER)}
+          groupBy={(option) => option.parent?.id || 'root'}
+          getOptionLabel={(option) => option.label}
+          isOptionEqualToValue={(option, value) => option.id === value.id}
+          renderInput={(params) => 
+            <TextField
+              {...params}
+              label="Gnomad File"
+              error={Boolean(gnomadError)}
+            />
+          }
+          renderGroup={(params) => (
+            <li key={params.key}>
+              <Box sx={{ px: '0.5rem' }}>
+                <StyledGroupParamsMenuItemTypographyBold>{`${params.group}:`}</StyledGroupParamsMenuItemTypographyBold>
+              </Box>
+              <StyledGroupParamsMenuItemTypography>{params.children}</StyledGroupParamsMenuItemTypography>
+            </li>
+          )}
+        />
       </Box>
-      <Box sx={{ height: '100%', display: 'grid', flexDirection: 'column', alignItems: 'start' }}>
-        <FormControl sx={{ width: '90%' }} size='small'>
-          <GroupParamsInputLabel label={'Save To'} />
-          <StyledGroupParamsSelect
-            id={'save-to-select'}
-            name={'save-to'}
-            label={'Save To'}
-            value={saveToValue}
-            onChange={handleSaveToChange}
-            disabled={blocked}
-            MenuProps={{
-              PaperProps: {
-                sx: {
-                  maxWidth: '15.7rem',
-                  maxHeight: '20rem',
-                  bgcolor: Theme.palette.background.default,
-                },
+      <Box sx={{ display: 'flex', flexDirection: 'column', rowGap: '1rem', width: '50%', flexGrow: '1'}}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', rowGap: '0.25rem'}}>
+          <Autocomplete
+            size="small"
+            sx={(theme) => ({
+              '& fieldset': {
+                borderColor: theme.palette.text.primary,
+                borderRadius: '1rem',
               },
-            }}
-          >
-            <StyledGroupParamsMenuItem key={'new-file'} value={'/'}>
-              <StyledGroupParamsMenuItemTypography>New file...</StyledGroupParamsMenuItemTypography>
-            </StyledGroupParamsMenuItem>
-            <StyledGroupParamsListSubheader key={'root'}>
-              <StyledGroupParamsMenuItemTypographyBold>root:</StyledGroupParamsMenuItemTypographyBold>
-            </StyledGroupParamsListSubheader>
-            {fileArray.map((file) => {
-              if (file.type === FileTypes.FOLDER) {
-                return (
-                  <StyledGroupParamsListSubheader key={file.id}>
-                    <StyledGroupParamsMenuItemTypographyBold>{file.id}:</StyledGroupParamsMenuItemTypographyBold>
-                  </StyledGroupParamsListSubheader>
-                );
-              }
-
-              return (
-                <StyledGroupParamsMenuItem key={file.id} value={file.id}>
-                  <StyledGroupParamsMenuItemTypography>{file.label}</StyledGroupParamsMenuItemTypography>
-                </StyledGroupParamsMenuItem>
-              );
             })}
-          </StyledGroupParamsSelect>
-        </FormControl>
-        {saveToValue !== '/' && (
-          <FormControlLabel
-            control={
-              <Checkbox
-                id='override-checkbox'
-                checked={overrideValue}
-                onChange={handleOverrideChange}
-                disabled={blocked}
+            value={saveToState}
+            onChange={(_event, value) => {
+              if (value)
+                handleSaveToChange(value)
+            }}
+            disabled={blocked}
+            options={[defaultSaveTo, ...fileArray.filter((file) => file.type !== FileTypes.FOLDER)]}
+            groupBy={(option) => option.parent?.id || 'root'}
+            getOptionLabel={(option) => option.label}
+            isOptionEqualToValue={(option, value) => option.id === value.id}
+            renderInput={(params) => 
+              <TextField
+                {...params}
+                label="Save To"
               />
             }
-            label={<GroupParamsTypography label={'Override File'} />}
-            labelPlacement='start'
+            renderGroup={(params) => (
+              <li key={params.key}>
+                <Box sx={{ px: '0.5rem' }}>
+                  <StyledGroupParamsMenuItemTypographyBold>{`${params.group}:`}</StyledGroupParamsMenuItemTypographyBold>
+                </Box>
+                <StyledGroupParamsMenuItemTypography>{params.children}</StyledGroupParamsMenuItemTypography>
+              </li>
+            )}
           />
-        )}
+          {saveToState.id !== defaultSaveTo.id && (
+            <FormControlLabel
+              control={
+                <Checkbox
+                  id='override-checkbox'
+                  checked={overrideState}
+                  onChange={handleOverrideChange}
+                  disabled={blocked}
+                />
+              }
+              label={<GroupParamsTypography label={'Override File'} />}
+              labelPlacement='start'
+            />
+          )}
+        </Box>
       </Box>
     </Box>
   );
